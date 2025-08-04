e8e326cd60324b86992d2527bd09486bc4eabeda
ceske-42 <ceske@etik.com>
2025-08-04 15:14:42 +0200

    erorri assurdi

--- sources/flood_fill.c
+++ sources/flood_fill.c
@@ -6,7 +6,7 @@
 /*   By: rceschel <rceschel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/29 16:15:00 by rceschel          #+#    #+#             */
-/*   Updated: 2025/08/04 13:25:18 by rceschel         ###   ########.fr       */
+/*   Updated: 2025/08/04 15:13:58 by rceschel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,46 +25,38 @@ typedef struct s_grid
 } t_grid;
 
 
-static bool floodfill(t_grid *g, int x, int y)
+static void floodfill(t_grid *g, int x, int y)
 {
-	bool result;
-
-    if (x < 0 || x >= g->width || y < 0 || y >= g->height)
-        return (false);
-    if (g->grid[y][x] == T_WALL || g->grid[y][x] == T_VOID)
-        return (false);
-    if (g->grid[y][x] == T_VISITED)
-        return (false);
-    if (g->grid[y][x] == T_EXIT)
-        return (true);
+	if (x < 0 || x >= g->width || y < 0 || y >= g->height)
+        return;
     g->grid[y][x] = T_VISITED;
-    result = (
-				floodfill(g, x + 1, y) ||
-				floodfill(g, x - 1, y) ||
-				floodfill(g, x, y + 1) ||
-				floodfill(g, x, y - 1)
-			);
-    return (result);
+	floodfill(g, x + 1, y);
+	floodfill(g, x - 1, y);
+	floodfill(g, x, y + 1);
+	floodfill(g, x, y - 1);
 }
 
 // I'll use the T_PLAYER value to sign the checked tiles 
-static bool not_floodfill(t_grid *g, int x, int y)
+static bool map_is_completable(t_grid *g, int x, int y)
 {
-	if (x < 0 || x >= g->width || y < 0 || y >= g->height)
-		return (false);
-    if (g->grid[y][x] == T_WALL)
-        return (false);
-	else if (g->grid[y][x] == T_PLAYER)
-		return (false);
-	else if (g->grid[y][x] == T_EXIT)
-        return (true);
-    g->grid[y][x] = T_PLAYER;
-    return (floodfill(g, x + 1, y) ||
-        floodfill(g, x - 1, y) ||
-        floodfill(g, x, y + 1) ||
-        floodfill(g, x, y - 1));
-}
+	int	i;
+	int	j;
 
+	floodfill(g, x, y);
+	i = 0;
+	while (i < g->height)
+	{
+		j = 0;
+		while (j < g->width)
+		{
+			if (g->grid[i][j] == T_EXIT || g->grid[i][j] == T_COLLECTIBLE)
+				return false;
+			j++;
+		}
+		i++;
+	}
+	return true;
+}
 static void	free_grid(t_grid *g)
 {
 	int	i;
@@ -114,6 +106,7 @@ bool flood_fill(t_map *map)
 	int												i;
 	t_grid __attribute__	((cleanup(free_grid)))	g;
 
+	ft_memset(&g, 0,  sizeof(t_grid));
 	g.height = map->height;
 	g.width = map->width;
 	g.grid = ft_calloc(map->height + 1, sizeof(t_tile *));
@@ -131,5 +124,6 @@ bool flood_fill(t_map *map)
 	}
 	g.grid[map->height] = NULL;
 	g.grid[map->player.y][map->player.x] = T_FLOOR;
-	return (floodfill(&g, map->player.x, map->player.y));
+	return (map_is_completable(&g, map->player.x, map->player.y));
 }
+
